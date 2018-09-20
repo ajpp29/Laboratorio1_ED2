7029834f8a21765a9e02568e637124c1acc06764
Angel Jimenez <angeljpp29@gmail.com>
2018-09-19 23:06:52 -0600

    Creación de la vista Huffman

---
+++ app/src/main/java/com/example/angel/laboratorio1_ed2/Huffman.java
@@ -0,0 +1,22 @@
+package com.example.angel.laboratorio1_ed2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+public class Huffman extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.huffman);
+
+
+    }
+
+
+}
--- app/src/main/java/com/example/angel/laboratorio1_ed2/MainActivity.java
+++ app/src/main/java/com/example/angel/laboratorio1_ed2/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.angel.laboratorio1_ed2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnHuffman;
+        Button btnLZW;
+
+        btnHuffman=(Button) findViewById(R.id.btnHuffman);
+        btnLZW=(Button) findViewById(R.id.btnLZW);
+
+        btnHuffman.setOnClickListener(view ->{
+            startActivity(new Intent(getApplicationContext(), Huffman.class));
+        });
+
+
     }
+
+
 }
